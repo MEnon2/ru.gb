86695c74f64e74e9f290bfec14193ea9cef48670
MEnon2 <menon2@live.ru>
2021-05-01 10:35:43 +0300

    lesson2

--- lessons/src/lesson2/MyArrayDataException.java
+++ lessons/src/lesson2/MyArrayDataException.java
@@ -5,4 +5,5 @@ public class MyArrayDataException extends RuntimeException {
     public MyArrayDataException(int i, int j, String el) {
         System.out.printf("Элемент массива с индексом {%s,%s} содержит символ (или строку) '%s', которую нельзя преобразовать в число! %n", i, j, el);
     }
+
 }
