99ec146cf4730e94c8821c61640202c466c2c675
Aleksandr <menon2@live.ru>
2021-05-08 10:56:52 +0300

    lesson5

---
+++ lessons/src/LessonApp.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class LessonApp {
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+
+    public static void main(String[] args) {
+
+        method1();
+
+        try {
+            method2();
+        } catch (InterruptedException ex) {
+            System.out.println("Всё-таки произошла какая-то ошибка )))");
+        }
+
+    }
+
+    public static void method1() {
+        float[] arr = new float[size];
+
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        long b = System.currentTimeMillis();
+
+        System.out.println(b - a);
+    }
+
+    public static void method2() throws InterruptedException {
+        float[] arr = new float[size];
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < a2.length; i++) {
+                a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        long b = System.currentTimeMillis();
+
+        System.out.println(b - a);
+    }
+
+}
